48b3df8341ac3ee3e818a92ee14631a8fae82ef1
FFZ <1501493678@qq.com>
2018-02-02 22:48:19 +0800

    修改首页

---
+++ src/js/iedxjson.js
@@ -0,0 +1,46 @@
+var index=[
+    {
+        id:001,
+        name:'TAG Heuer/泰格豪雅',
+        intrduct:'创意无限的高端手表',
+        imgurl:'img/goods1.jpg'
+
+    },
+    {
+        id:002,
+        name:'POMELLATO/宝曼兰朵',
+        intrduct:'烂漫奢华的有色宝石',
+        imgurl:'img/goods2.jpg'
+
+    },
+    {
+        id:003,
+        name:'Bedat & CO /宝达家',
+        intrduct:'典雅大方的瑞士手表',
+        imgurl:'img/goods3.jpg'
+
+    },
+    {
+        id:004,
+        name:'Sergio Rossi/塞乔罗西',
+        intrduct:'力臻完美的意大利鞋靴',
+        imgurl:'img/goods4.jpg'
+
+    },
+    {
+        id:005,
+        name:'Mr & Mrs Italy/皮草夫妇',
+        intrduct:'潮流时尚的派克大衣',
+        imgurl:'img/goods5.jpg'
+
+    },
+    {
+        id:006,
+        name:'LA PERLA/萝贝拉',
+        intrduct:'意大利高端内衣',
+        imgurl:'img/goods6.jpg'
+
+    }
+]
+var res=JSON.stringify(index);
+console.log(res);
---
+++ src/lib/carousel/carousel.js
@@ -0,0 +1,177 @@
+'use strict';
+;(function($){
+    $.fn.carousel = function(options){
+        var defaults = {
+            index:0,
+            width:400,
+            height:260,
+            duration:3000,
+            autoPlay:true,
+            page:true,
+            seamless:true,
+            type:'horizontal', //fade,vertical
+            pn:true   
+        }
+
+        return this.each(function(idx,item){
+            var opt = $.extend({},defaults,options);
+            // opt.len = opt.imgs.length;
+            var $this = $(this);
+            $this.addClass('box');
+            $this.css({
+                width:opt.width,
+                height:opt.height
+            });
+            var len;
+            var $ul;
+            init();
+            function init(){
+                $ul = $('<ul/>');
+                var html = $.map(opt.imgs,function(item){
+                    return '<li><img src="'+item+'"/></li>'
+                }).join('\n');
+                $ul.html(html).appendTo($this);
+
+
+                if(opt.seamless){
+                    var $li = $('<li/>')
+                    $li=$ul.children('li').first().clone();
+                    $ul.append($li);
+                }
+                len = $ul.children('li').length;
+                $ul.css('width',len*opt.width);
+
+                if(opt.autoPlay){
+                    move();
+                }
+
+                if(opt.pn){
+                    var $prev=$('<div/>');
+                    $prev.addClass('zuo');
+                    $prev.text('<');
+                    $prev.appendTo($this);
+                    $this.on('click','.zuo',function(){
+                        opt.index--;
+                        bian();            
+                    });
+                    var $next=$('<div/>');
+                    $next.addClass('you');
+                    $next.text('>');
+                    $next.appendTo($this);
+                    $this.on('click','.you',function(){
+                        opt.index++;
+                        bian();            
+                    });
+                }
+                if(opt.page){
+                    if(opt.seamless){
+                        var $page = $('<div/>');
+                        $page.addClass('page');
+                        for(var i=1;i<len;i++){
+                            var $span = $('<span/>');
+                            $span.addClass('sp');
+                            $span.text(i);
+                            $page.append($span);
+                        }
+                        $this.append($page);
+                        var $spans=$('.sp');
+                        $spans.first().addClass('active');
+                        $this.on('click','span',function(){
+                            opt.index=this.innerHTML-1;
+                            bian();
+                        });
+                     }else{
+                        var $page = $('<div/>');
+                        $page.addClass('page');
+                        for(var i=1;i<=len;i++){
+                            var $span = $('<span/>');
+                            $span.addClass('sp');
+                            $span.text(i);
+                            $page.append($span);
+                        }
+                        $this.append($page);
+                        var $spans=$('.sp');
+                        $spans.first().addClass('active');
+                        $this.on('click','span',function(){
+                            opt.index=this.innerHTML-1;
+                            bian();
+                        });
+                    }   
+                }
+
+                $this.mouseenter(function(){
+                    if(opt.autoPlay){
+                        clearInterval(opt.timer);       
+                    }
+                    if(opt.pn){
+                        $('.zuo').css('display','block');
+                        $('.you').css('display','block');
+                    } 
+                });
+                $this.mouseleave(function(){
+                    if(opt.autoPlay){
+                        opt.timer=setInterval(()=>{
+                            show();
+                        },opt.duration);
+                    }
+                    if(opt.pn){
+                        $('.zuo').css('display','none');
+                        $('.you').css('display','none');
+                    } 
+                });
+            }   
+
+            function move(){
+                opt.timer = setInterval(()=>{
+                    show();
+                },opt.duration);
+            }
+            function show(){
+                opt.index++;
+                bian();
+            }
+            function bian(){
+                if(opt.seamless){
+                    if(opt.index>=len){
+                        opt.index=1;
+                        $ul.css('left',0);
+                        
+                    }else if(opt.index<0){
+                        opt.index = len-2;
+                        $ul.css('left',-opt.width*(len-1));   
+                    }    
+                }else{
+                    if(opt.index>=len){
+                        opt.index=0;
+                        
+                    }else if(opt.index<0){
+                        opt.index = len-1;     
+                    }   
+                }
+                var target = -opt.width*opt.index;
+                $ul.stop().animate({'left':target});
+                if(opt.page){
+                    var $span = $('.sp');
+                    $.each($span,function(idx,item){
+                        $(item).removeClass('active');
+                    });
+                    if(opt.seamless){
+                        if(opt.index<len-1){
+                            console.log($span.eq(opt.index))
+                                 
+                            $span.eq(opt.index).addClass('active');
+                        }else{
+                            $span.first().addClass('active');
+                        }
+                    }else{
+                        if(opt.index<len){
+                            $span.eq(opt.index).addClass('active');
+                        }else{
+                            $span.first().addClass('active');
+                        } 
+                    }
+                }
+            }   
+        });
+    }
+})(jQuery);
