01e066c5c9e481f7b0625f37b97738c284612f89
FFZ <1501493678@qq.com>
2018-02-04 00:23:29 +0800

    再次完善注册登录页

---
+++ src/js/zhuce.js
@@ -0,0 +1,10 @@
+jQuery(function($){
+    var $username=$('#username');
+    var $password=$('#password');
+    var $qr      =$('#qr');
+    var $yzm     =$('#yzm');
+    var $dx      =$('#dx');
+    var $agree   =$('#agree');
+    var $zc_btn  =$('.zc_btn');
+    
+})
